eccc0dd85a7648a7f907dc5cc496961f04728130
Kadir Çetin <kadirrcetin@gmail.com>
2023-04-09 07:25:06 +0300

    090403-2

---
+++ AppNETKdr/AppNETKdr.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using AppNETKdr.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNETKdr.Domain.Entities
+{
+    public class Category:BaseEntity
+    {
+        public string Name { get; set; }
+       // public List<Product> Products { get; set; }
+    }
+}
---
+++ AppNETKdr/AppNETKdr.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using AppNETKdr.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNETKdr.Domain.Entities
+{
+    public class Product:AuditEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal BuyPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
