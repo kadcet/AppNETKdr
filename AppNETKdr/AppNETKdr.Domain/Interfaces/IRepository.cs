9e9d58defca7548035e1f3ca19bccf26233c70fd
Kadir Çetin <kadirrcetin@gmail.com>
2023-04-10 01:05:25 +0300

    100123-1

---
+++ AppNETKdr/AppNETKdr.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using AppNETKdr.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNETKdr.Domain.Interfaces
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        T Add(T entity);
+        bool Remove(int id);
+        T GetById(int id);
+        ICollection<T> GetList(Func<T, bool> expression = null);
+        T Update(int id,T entity);
+        
+    }
+}
---
+++ AppNETKdr/AppNETKdr.Infrastructure.IOToTXT/TextFileRepository.cs
@@ -0,0 +1,91 @@
+﻿using AppNETKdr.Domain.Entities.Base;
+using AppNETKdr.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppNETKdr.Infrastructure.IOToTXT
+{
+    public class TextFileRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        #region Dosya Adı
+        private static string FileName
+        {
+            get
+            {
+                return typeof(T).FullName.Replace(".", "") + "txt";
+            }
+        } 
+        #endregion
+
+        #region Liste
+        private static List<T> list = new List<T>();
+        #endregion
+
+        #region Dosyadan Okuma Metodu
+        private static void LoadListFromFile()
+        {
+            if (!File.Exists(FileName))
+            {
+                list = new List<T>();
+                return;
+            }
+
+            var json = File.ReadAllText(FileName);
+            list = JsonSerializer.Deserialize<List<T>>(json);
+
+        }
+        #endregion
+
+        #region Dosyaya Yazma Metodu
+        private static void WriteListToTxt()
+        {
+            var jsonText = JsonSerializer.Serialize(list);
+            File.WriteAllText(FileName, jsonText);
+        } 
+        #endregion
+
+        #region static Yapıcı Metod Sadece 1 Kere Çalışıcak
+        static TextFileRepository()
+        {
+            LoadListFromFile();
+        } 
+        #endregion
+
+        public T Add(T entity)
+        {
+            LoadListFromFile();
+            list.Add(entity);
+            WriteListToTxt();
+            return entity;
+        }
+
+        public T GetById(int id)
+        {
+            LoadListFromFile();
+            var entity = list.FirstOrDefault(x => x.Id == id);
+            return entity;
+            
+        }
+
+        public ICollection<T> GetList(Func<T, bool> expression = null)
+        {
+            LoadListFromFile();
+            //expre yoksa listeyi ?(null)dön :değil ise bunu dön
+            return expression == null ? list : list.Where(expression).ToList();
+        }
+
+        public bool Remove(int id)
+        {
+            LoadListFromFile();
+        }
+
+        public T Update(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
